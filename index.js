da65da35a2f72f9cea16a879a318ef1e5e0e8273
vivekrajput-93 <viveksinghraajputt@gmail.com>
2024-06-21 11:59:32 +0530

    code refactor

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const bodyParser = require("body-parser");
 const connectDB = require("./config/db")
 const apiRoutes  = require("./routes/index");
 const cors = require("cors")
+const path = require("path");
+
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // Database connection
 connectDB();
+
+app.use('*', function(req,res) {
+    const indexPath = path.resolve(__dirname,  'client', 'build', 'index.html');
+res.sendFile(indexPath);
+
+})
 
 // routes connection
 app.use("/api", apiRoutes);
